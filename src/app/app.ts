d5c57e51ed9aeb1762aad84d0fb3e3d3d31f42f0
Guenter Heldt <gh1978@hotmail.de>
2026-08-11 13:38:02 +0200

    feat: set up initial component and styling structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CreateSurvey } from './shared/components/create-survey/create-survey';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CreateSurvey],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('poll_app');
---
+++ src/app/shared/components/survey-card/survey-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-card',
+  imports: [],
+  templateUrl: './survey-card.html',
+  styleUrl: './survey-card.scss',
+})
+export class SurveyCard {}
