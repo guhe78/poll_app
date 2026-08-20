4cfe61c7e4121477788686681e30e5da1178e72a
Guenter Heldt <gh1978@hotmail.de>
2026-08-20 11:27:08 +0200

    feat: button style icon

--- src/app/shared/components/create-survey/create-survey.ts
+++ src/app/shared/components/create-survey/create-survey.ts
@@ -1,5 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { SurveyForm } from '../survey-form/survey-form';
+import { uiIcons } from '../../../assets/icons';
+
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-create-survey',
@@ -7,4 +10,10 @@ import { SurveyForm } from '../survey-form/survey-form';
   templateUrl: './create-survey.html',
   styleUrl: './create-survey.scss',
 })
-export class CreateSurvey {}
+export class CreateSurvey {
+  private sanitizer = inject(DomSanitizer);
+
+  readonly icons = {
+    check: this.sanitizer.bypassSecurityTrustHtml(uiIcons.check()),
+  };
+}
